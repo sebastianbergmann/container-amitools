3004e616cd9dfb1f6ad263cdeb7d462d491f4125
Sebastian Bergmann <sb@sebastian-bergmann.de>
2018-03-30 10:37:17 +0200

    Refactor

--- bump.php
+++ bump.php
@@ -1,16 +1,11 @@
 #!/usr/bin/env php
 <?php
-$sha1   = trim(`git ls-remote https://github.com/cnvogelg/amitools.git | grep HEAD | awk '{ print $1}'`);
-$buffer = '';
-$prefix = '    git checkout -qf ';
+$tmp = file_get_contents(__DIR__ . '/Dockerfile');
+$old = substr($tmp, strpos($tmp, 'git checkout -qf ') + strlen('git checkout -qf '), 40);
+$new = trim(`git ls-remote https://github.com/cnvogelg/amitools.git | grep HEAD | awk '{ print $1}'`);
 
-foreach (file(__DIR__ . '/Dockerfile') as $line) {
-    if (strpos($line, $prefix) === 0) {
-        $line = $prefix . $sha1 . ' && \\' . PHP_EOL;
-    }
-
-    $buffer .= $line;
-}
-
-file_put_contents(__DIR__ . '/Dockerfile', $buffer);
+file_put_contents(
+    __DIR__ . '/Dockerfile',
+    str_replace($old, $new, $tmp)
+);
 
